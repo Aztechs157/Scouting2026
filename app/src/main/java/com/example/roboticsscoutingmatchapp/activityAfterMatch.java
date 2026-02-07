8526950a49f55249ecb93f223492c616dd4c0281
Leo <everythingchannel0323@gmail.com>
2026-02-07 10:29:32 -0500

    updated post match xml and java -LQ

--- app/src/main/java/com/example/roboticsscoutingmatchapp/activityAfterMatch.java
+++ app/src/main/java/com/example/roboticsscoutingmatchapp/activityAfterMatch.java
@@ -39,8 +39,8 @@ public class activityAfterMatch extends AppCompatActivity {
 
         // *---Defines all the components on the current page as variables---*
 
-        CheckBox floorIntake = findViewById(R.id.floor_intake);
-        CheckBox humanPlayerStation = findViewById(R.id.human_player_intake);
+        CheckBox overBump = findViewById(R.id.over_bump);
+        CheckBox underTrench = findViewById(R.id.under_trench);
 
         RadioGroup stopReasonGroup = findViewById(R.id.why_robot_stopped);
         RadioButton diedButton = findViewById(R.id.died);
@@ -83,10 +83,10 @@ public class activityAfterMatch extends AppCompatActivity {
         if(!postMatchSaveString.isEmpty()){ // Sets all the components to the values within the savestring
             // Coral floor pickup able | Coral Source pickup able | Defense received |
             // Stop reason | team rank among alliance | other comments questions or concerns ||
-            floorIntake.setChecked(Boolean.parseBoolean(u.untilNextComma(postMatchSaveString))); // Sets the value to the parsed value in the savestring
+            overBump.setChecked(Boolean.parseBoolean(u.untilNextComma(postMatchSaveString))); // Sets the value to the parsed value in the savestring
             postMatchSaveString = u.nextCommaOn(postMatchSaveString); // removes the value from the savestring
             // So on an so forth
-            humanPlayerStation.setChecked(Boolean.parseBoolean(u.untilNextComma(postMatchSaveString)));
+            underTrench.setChecked(Boolean.parseBoolean(u.untilNextComma(postMatchSaveString)));
             postMatchSaveString = u.nextCommaOn(postMatchSaveString);
 
             if(u.untilNextComma(postMatchSaveString).equals("No Defense")){
@@ -142,8 +142,8 @@ public class activityAfterMatch extends AppCompatActivity {
             // Stop reason | team rank among alliance | other comments questions or concerns ||
             String afterMatchInfo = "";
 
-            afterMatchInfo += u.getData(floorIntake) + ",";
-            afterMatchInfo += u.getData(humanPlayerStation) + ",";
+            afterMatchInfo += u.getData(underTrench) + ",";
+            afterMatchInfo += u.getData(overBump) + ",";
             afterMatchInfo += u.getData(defenseReceivedGroup) + ",";
             afterMatchInfo += u.getData(stopReasonGroup) + ",";
             afterMatchInfo += u.getData(rankGroup) + ",";
@@ -171,8 +171,8 @@ public class activityAfterMatch extends AppCompatActivity {
                 // Coral floor pickup able | Coral Source pickup able | Defense received |
                 // Stop reason | team rank among alliance | other comments questions or concerns ||
                 String postMatchInfo = "";
-                postMatchInfo += u.getData(floorIntake) + ",";
-                postMatchInfo += u.getData(humanPlayerStation) + ",";
+                postMatchInfo += u.getData(overBump) + ",";
+                postMatchInfo += u.getData(underTrench) + ",";
                 postMatchInfo += u.getData(defenseReceivedGroup) + ",";
                 postMatchInfo += u.getData(stopReasonGroup) + ",";
                 postMatchInfo += u.getData(rankGroup) + ",";
